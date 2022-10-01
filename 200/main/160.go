147b7a6a7e53572a81ea8df3ab6ddf045653a07e
wikyyuen <wikyyuen@gmail.com>
2022-10-02 00:58:59 +0800

    解答160

---
+++ 200/main/160.go
@@ -0,0 +1,32 @@
+package main
+
+//160. Intersection of Two Linked Lists
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func getIntersectionNode(headA, headB *ListNode) *ListNode {
+	p1 := headA
+	p2 := headB
+	for p1 != p2 {
+		if p1 == nil {
+			p1 = headB
+		} else {
+			p1 = p1.Next
+		}
+		if p2 == nil {
+			p2 = headA
+		} else {
+			p2 = p2.Next
+		}
+	}
+	return p1
+}
+
+func main() {
+
+}
