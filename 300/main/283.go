8aef36e48935d4088c38e870dc08de44664a215e
wikyyuen <wikyyuen@gmail.com>
2022-10-05 23:56:06 +0800

    解答283

---
+++ 300/main/283.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+//283. Move Zeroes
+
+func moveZeroes(nums []int) []int {
+	return nums
+
+}
+
+func main() {
+	nums := []int{1, 2, 0, 4}
+
+	result := moveZeroes(nums)
+
+	fmt.Println(result)
+}
