b335150d00b257ef9c491d8836c50d6f6716fc3e
wikyyuen <wikyyuen@gmail.com>
2022-10-06 10:09:01 +0800

    更新283

--- 300/main/283.go
+++ 300/main/283.go
@@ -3,16 +3,36 @@ package main
 import "fmt"
 
 //283. Move Zeroes
+//solution1
+//func moveZeroes(nums []int) []int {
+//	n := 0
+//	for i := 0; i < len(nums); i++ {
+//		if nums[i] == 0 {
+//			nums = append(nums[:i], nums[i+1:]...)
+//			i--
+//			n++
+//		}
+//	}
+//	addArr := make([]int, n)
+//	nums = append(nums, addArr...)
+//	return nums
+//}
+//solution2
+func moveZeroes(nums []int) {
+	index := 0
 
-func moveZeroes(nums []int) []int {
-	return nums
-
+	for i := range nums {
+		if nums[i] != 0 {
+			nums[index], nums[i] = nums[i], nums[index]
+			index++
+		}
+	}
 }
 
 func main() {
-	nums := []int{1, 2, 0, 4}
+	nums := []int{0, 1, 0, 3, 12}
 
-	result := moveZeroes(nums)
+	moveZeroes(nums)
 
-	fmt.Println(result)
+	fmt.Println(nums)
 }
