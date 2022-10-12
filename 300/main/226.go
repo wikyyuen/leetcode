3d0317334164ca65589a713541e89c2511129a31
wikyyuen <wikyyuen@gmail.com>
2022-10-12 20:45:01 +0800

    解决226

---
+++ 300/main/226.go
@@ -0,0 +1,21 @@
+package main
+
+//226. Invert Binary Tree
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	root.Left, root.Right = invertTree(root.Right), invertTree(root.Left)
+	return root
+}
+
+func main() {
+
+}
