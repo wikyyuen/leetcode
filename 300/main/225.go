49ccdd22aea1b51396f37fe2411369c1a14900b7
wikyyuen <wikyyuen@gmail.com>
2022-11-24 10:29:33 +0800

    优化225

--- 300/main/225.go
+++ 300/main/225.go
@@ -5,26 +5,32 @@ package main
 
 type MyStack struct {
 	items []int
+	lens  int
 }
 
 func Constructor() MyStack {
-	return MyStack{items: []int{}}
+	stack := new(MyStack)
+	stack.lens = 0
+	stack.items = make([]int, 0)
+	return *stack
 }
 
 func (this *MyStack) Push(x int) {
 	this.items = append(this.items, x)
+	this.lens += 1
 }
 
 func (this *MyStack) Pop() int {
-	x := this.items[len(this.items)-1]
-	this.items = this.items[:len(this.items)-1]
+	x := this.items[this.lens-1]
+	this.items = this.items[:this.lens-1]
+	this.lens -= 1
 	return x
 }
 
 func (this *MyStack) Top() int {
-	return this.items[len(this.items)-1]
+	return this.items[this.lens-1]
 }
 
 func (this *MyStack) Empty() bool {
-	return len(this.items) == 0
+	return this.lens == 0
 }
