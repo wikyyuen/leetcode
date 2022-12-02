8ba688f5d0c29b9dfa62e51b885cb959c8c51368
wikyyuen <wikyyuen@gmail.com>
2022-12-02 11:45:30 +0800

    优化752，双向bfs

--- 800/main/752.go
+++ 800/main/752.go
@@ -6,27 +6,32 @@ import (
 
 //752. Open the Lock
 //752. 打开转盘锁
-//BFS
+//双向BFS
 func openLock(deadends []string, target string) int {
 	deadMap := map[string]bool{}
 	visitedMap := map[string]bool{}
+	backVisitedMap := map[string]bool{}
 	step := 0
 	for i := 0; i < len(deadends); i++ {
 		deadMap[deadends[i]] = true
 	}
-	res := []string{}
-	res = append(res, "0000")
+	forward := []string{}
+	forward = append(forward, "0000")
 	visitedMap["0000"] = true
 
-	for len(res) > 0 {
-		n := len(res)
+	backward := []string{}
+	backward = append(backward, target)
+	backVisitedMap[target] = true
+
+	for len(forward) > 0 || len(backward) > 0 {
+		n := len(forward)
 		for i := 0; i < n; i++ {
-			cur := res[0]
-			res = res[1:]
+			cur := forward[0]
+			forward = forward[1:]
 			if _, ok := deadMap[cur]; ok {
 				continue
 			}
-			if cur == target {
+			if _, ok := backVisitedMap[cur]; ok {
 				return step
 			}
 			for j := 0; j < 4; j++ {
@@ -34,12 +39,37 @@ func openLock(deadends []string, target string) int {
 				next := upOne(cur, j)
 				if _, ok := visitedMap[next]; !ok {
 					visitedMap[next] = true
-					res = append(res, next)
+					forward = append(forward, next)
 				}
 				next = downOne(cur, j)
 				if _, ok := visitedMap[next]; !ok {
 					visitedMap[next] = true
-					res = append(res, next)
+					forward = append(forward, next)
+				}
+			}
+		}
+		step++
+		n2 := len(backward)
+		for i := 0; i < n2; i++ {
+			curBack := backward[0]
+			backward = backward[1:]
+			if _, ok := deadMap[curBack]; ok {
+				continue
+			}
+			if _, ok := visitedMap[curBack]; ok {
+				return step
+			}
+			for j := 0; j < 4; j++ {
+				//每一位处理两次
+				next := upOne(curBack, j)
+				if _, ok := backVisitedMap[next]; !ok {
+					backVisitedMap[next] = true
+					backward = append(backward, next)
+				}
+				next = downOne(curBack, j)
+				if _, ok := backVisitedMap[next]; !ok {
+					backVisitedMap[next] = true
+					backward = append(backward, next)
 				}
 			}
 		}
